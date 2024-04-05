a76d24e7b0c311cbc04971e9fff4198755383f9f
Marek Roque <marekroque58@gmail.com>
2024-04-05 15:36:19 +0200

    Added functionality to add everything to one reactantDict and productDict

--- Project Engineering.py
+++ Project Engineering.py
@@ -5,22 +5,49 @@ import re
 # reactant variables
 react = input("Enter the reactants: ")
 reactantList = react.split()
-reactListNumber = 0
+i = 0
 
 # product variables
 product = input("Enter the produts: ")
 productList = product.split()
 productListNumber = 0
-print(productList)
 
-# Prints the full reaction 
-reactantString = re.sub(" ", " + ", react)
-productString = re.sub(" ", " + ", product)
-print("Reaction: ", reactantString, "-->", productString)
+# Prints the full reaction
+if "+" not in react or product:
+    reactantString = re.sub(" ", " + ", react)
+    productString = re.sub(" ", " + ", product)
+    print("Reaction: ", reactantString, "-->", productString)
+else:
+    print("Reaction: ", react, "-->", product)
 
 # For loop adds all the reactants to a dictionary
-for x in reactantList:
-    reactantDict = chemparse.parse_formula(reactantList[reactListNumber])
-    print(reactantDict)
-    reactListNumber = reactListNumber + 1
+reactantDict = {}
+# Iterate over each reactant in the reactantList
+for react in reactantList:
+    reactantValues = chemparse.parse_formula(react)
+    # Update the reactantDict with the parsed formula
+    for key, value in reactantValues.items():
+        if key in reactantDict:
+            reactantDict[key] += value
+        else:
+            reactantDict[key] = value
+if "+" in reactantDict:
+    reactantDict.pop("+")
+print(reactantDict)
+
+# For loop adds all the reactants to a dictionary
+productDict = {}
+# Iterate over each reactant in the reactantList
+for product in productList:
+    productValues = chemparse.parse_formula(product)
+    # Update the reactantDict with the parsed formula
+    for key, value in productValues.items():
+        if key in productDict:
+            productDict[key] += value
+        else:
+            productDict[key] = value
+
+if "+" in productDict:
+    productDict.pop("+")
+print(productDict)
 
