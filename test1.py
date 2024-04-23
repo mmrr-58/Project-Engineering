e7f72191ef2d8c1747813926a26638af38198916
Marek Roque <marekroque58@gmail.com>
2024-04-23 14:42:39 +0200

    Added support for coefficient multiplication

--- test1.py
+++ test1.py
@@ -3,6 +3,18 @@ matrix1 = []
 matrix2 = []
 resultMatrix = []
 
+# Function to convert a single list a 2D matrix
+# Need to add support for 2x2 matrices
+def matrixConversion(matrix):
+    row = []
+    convertedMatrix = []
+    for value in matrix:
+        row.append(value)
+        if len(row) == 3:
+            convertedMatrix.append(row)
+            row = []
+    return convertedMatrix
+
 # Function for adding 2 matrices
 def matrixAdd():
     for x in range(0 , len(matrix1)):
@@ -16,27 +28,30 @@ def matrixSub():
         resultMatrix.append(result)
 
 # Function for multiplying with a coefficient
-'''def scalarMult():
-    for x in range(0, len(matrix1))
+def scalarMult():
+    for x in range(0, len(matrix1)):
         result = coefficient * matrix1[x]
-'''
+        resultMatrix.append(result)
+
 # Function for Matrix multiplication
 def matrixMult():
-    row = []
-    matrixMult1 = []
-    for value in matrix1:
-        row.append(value)
-        if len(row) == 3:
-            matrixMult1.append(row)
-            row = []
-    matrixMult2 = []
-    for value in matrix2:
-        row.append(value)
-        if len(row) == 3:
-            matrixMult2.append(row)
-            row = []
-    print(matrixMult1)
-    print(matrixMult2)
+    m1 = matrixConversion(matrix1)
+    m2 = matrixConversion(matrix2)
+    print(m1)
+    print(m2)
+    row = 0
+    item = row
+    transList = []
+    x = 0 
+    while row < 2:
+        print(row)
+        for value in m1[row]:
+            transNumber = value * m2[x][item]
+            x = x + 1
+            transList.append(transNumber)
+        number = sum(transList)
+        resultMatrix.append(number)
+        row = row + 1
 
 # Define operation and size
 operationChoice = int(input("Choose the operation to be performed \n 1. Addition \n 2. Substraction \n 3. Scalar Multiplication \n 4. Matrix Multiplication \n"))
@@ -55,17 +70,26 @@ while x < 9:
     number = int(input("Input number: "))
     matrix1.append(number)    
     x = x + 1
-print("Input the values as they appear in rows, starting with a1")
-while y < 9:    
-    number = int(input("Input number: "))
-    matrix2.append(number)    
-    y = y + 1
+if operationChoice == 3:
+    coefficient = int(input("Input coefficient: "))
+else:
+    print("Input the values as they appear in rows, starting with a1")
+    while y < 9:    
+        number = int(input("Input number: "))
+        matrix2.append(number)    
+        y = y + 1
 
 if operationChoice == 1:
     matrixAdd()
 elif operationChoice == 2:
     matrixSub()
+elif operationChoice == 3:
+    scalarMult()
 elif operationChoice == 4:
+    matrixConversion(matrix1)
+    matrixConversion(matrix2)
     matrixMult()
 else:
     print("Needs to be developed")
+
+print(matrixConversion(resultMatrix))
