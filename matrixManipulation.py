0dd25de8b070983cd946984b546420746c92f9cd
Marek Roque <marekroque58@gmail.com>
2024-04-23 12:34:07 +0200

    Don't want to lose progress. Did some changes for the matrix multiplication

--- matrixManipulation.py
+++ matrixManipulation.py
@@ -1,40 +1,71 @@
+# Define variables needed
 matrix1 = []
 matrix2 = []
 resultMatrix = []
 
+# Function for adding 2 matrices
 def matrixAdd():
     for x in range(0 , len(matrix1)):
-        print(x)
         result = matrix1[x] + matrix2[x]
         resultMatrix.append(result)
 
+# Function for subtracting 2 matrices
 def matrixSub():
     for x in range(0 , len(matrix1)):
-        print(x)
         result = matrix1[x] - matrix2[x]
         resultMatrix.append(result)
 
-operationChoice = int(input("Choose the operation to be performed \n 1.Addition \n 2. Substraction \n 3. Constant Multiplication \n 4.Matrix Multiplication"))
+# Function for multiplying with a coefficient
+'''def scalarMult():
+    for x in range(0, len(matrix1))
+        result = coefficient * matrix1[x]
+'''
+# Function for Matrix multiplication
+def matrixMult():
+    row = []
+    matrixMult1 = []
+    for value in matrix1:
+        row.append(value)
+        if len(row) == 3:
+            matrixMult1.append(row)
+            row = []
+    matrixMult2 = []
+    for value in matrix2:
+        row.append(value)
+        if len(row) == 3:
+            matrixMult2.append(row)
+            row = []
+    print(matrixMult1)
+    print(matrixMult2)
 
+# Define operation and size
+operationChoice = int(input("Choose the operation to be performed \n 1. Addition \n 2. Substraction \n 3. Scalar Multiplication \n 4. Matrix Multiplication \n"))
+matrixSizeChoice = int(input("Choose the dimension of the matrices: \n 1. 2x2 \n 2. 3x3 \n"))
 print("Input the values as they appear in rows, starting with a1")
-x = 0
+if matrixSizeChoice == 1:
+    x = 5
+elif matrixSizeChoice == 2:
+    x = 0
+else:  
+    print("Select a valid option")
+
+# Creatre the two matrices on which to perform operations
+y = x
 while x < 9:    
     number = int(input("Input number: "))
     matrix1.append(number)    
     x = x + 1
-print(matrix1)
 print("Input the values as they appear in rows, starting with a1")
-x = 0
-while x < 9:    
+while y < 9:    
     number = int(input("Input number: "))
     matrix2.append(number)    
-    x = x + 1
-print(matrix2)
+    y = y + 1
 
 if operationChoice == 1:
     matrixAdd()
 elif operationChoice == 2:
     matrixSub()
+elif operationChoice == 4:
+    matrixMult()
 else:
     print("Needs to be developed")
-print(resultMatrix)
--- test.py
+++ test.py
@@ -1,65 +1,14 @@
-''' Here is an example of how we can add all the parts of a string
+currentlist = []
+for w in input1:
+    currentlist.append(w)
+    if len(currentlist) == 3:
+        matrix1.append(currentlist)
+        currentlist = []
+print(matrix1)
 
-myString = input("String: ")
-myList = myString.split()
-
-print(myList)
-print(myList[2])
-
-result = int(myList[2]) + int(myList[4])
-print(result)
-
-'''
-'''
-# Python code to merge dict using update() method
-def Merge(dict1, dict2):
-    return(dict2.update(dict1))
- 
-# Driver code
-dict1 = {'a': 10, 'b': 8}
-dict2 = {'d': 6, 'c': 4}
- 
-# This returns None
-print(Merge(dict1, dict2))
- 
-# changes made in dict2
-print(dict2)
-'''
-''' Example of how you can add the values of dictionaries
-dic1 = {"name" : 3 , "age": 5 , "girlfriend": 1, "money": 2}
-dic2 = {"name" : 1 , "age": 1 , "girlfriend": 1, "status": 4}
-dic3 = { }
-for key in dic1.keys():
-    if (key in dic2.keys()):
-         dic2[key] = dic2[key]+ dic1[key]
-    else:
-        dic2[key]= dic1[key]
-print(dic2)
-'''
-# Import Library
-import chemparse
-import re
-
-# reactant variables
-react = input("Enter the reactants: ")
-reactantList = react.split()
-reactListNumber = 0
-
-# product variables
-product = input("Enter the produts: ")
-productList = product.split()
-productListNumber = 0
-
-# Prints the full reaction 
-reactantString = re.sub(" ", " + ", react)
-productString = re.sub(" ", " + ", product)
-print("Reaction: ", reactantString, "-->", productString)
-
-# For loop adds all the reactants to a dictionary
-reactDicList = []
-for x in reactantList:
-    reactantDict = chemparse.parse_formula(reactantList[reactListNumber])
-    reactDicList.append(reactantDict)
-    reactListNumber = reactListNumber + 1
-
-print(type(reactDicList[0]))
+ row = []
+    for w in interList:
+        row.append(w)
+        if len(row) == 3:
+            matrix1.append(row)
+            row = []
